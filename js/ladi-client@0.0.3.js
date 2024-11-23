d251f77173a048e283d8d70fe41fec37ac3572d8
PhucNguyen2304 <nguyenngocphuck98@gmail.com>
2024-11-23 09:21:20 +0700

    update

--- js/ladi-client@0.0.3.js
+++ js/ladi-client@0.0.3.js
@@ -87,15 +87,17 @@ import { getDatabase, ref, child, get, set, update, remove } from "https://www.g
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDsnelmBqcMkRnVCNY31rSKnSPdfrXFqD0",
-    authDomain: "ladi-server.firebaseapp.com",
-    projectId: "ladi-server",
-    storageBucket: "ladi-server.appspot.com",
-    messagingSenderId: "17377594943",
-    appId: "1:17377594943:web:8fb81d318bc3a41446f28a",
-    measurementId: "G-8CP8ZLM5KK"
-};
+    apiKey: "AIzaSyC2NP7LZ_XiMIX7V2N6D6epWMbMNgn7WDQ",
+    authDomain: "ladi-server-f17d1.firebaseapp.com",
+    databaseURL: "https://ladi-server-f17d1-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ladi-server-f17d1",
+    storageBucket: "ladi-server-f17d1.firebasestorage.app",
+    messagingSenderId: "936327524202",
+    appId: "1:936327524202:web:73e3e6d2a32ec73d567f13",
+    measurementId: "G-0R6M2SB80T"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
